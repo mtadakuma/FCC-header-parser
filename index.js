53c458dde6641c719bc5427c6928a1a8ef3a6ceb
Replit user <>
2023-07-25 23:22:46 +0000

    project complete

--- index.js
+++ index.js
@@ -15,16 +15,23 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let ip = req.ip
+  let language = req.headers['accept-language']
+  let software = req.headers['user-agent']
+  res.json({ 'ipaddress': ip, 'language': language, 'software': software })
+})
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
